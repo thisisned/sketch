8e6a3cfe860e4c0145da2d3ffea17f1a8dcd06b0
Ned <ned.wilson.eames@gmail.com>
2023-01-08 12:37:56 +0000

    Refactored away from jQuery to vanilla JS

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # sketch
 An online sketching thing with several EXCITING OPTIONS
 
-Part of the 'learn Javascript / jQuery' part of The Odin Project's [curriculum](http://www.theodinproject.com/courses/web-development-101/lessons/javascript-and-jquery).
+Part of The Odin Project's [foundations](https://www.theodinproject.com/lessons/foundations-etch-a-sketch) course.
 
 Visible online [here](https://thisisned.github.io/sketch/).
--- js/script.js
+++ js/script.js
@@ -1,62 +1,73 @@
-res=16;
+// Initialisation
 
-$(document).ready(function(){
-	gridinit(res);
-	startbasic(); // On page load start the basic grid with size 16
+const coloursbtn = document.querySelector('#coloursbtn');
+const basicbtn = document.querySelector('#basicbtn');
+const fadebtn = document.querySelector('#fadebtn');
+const sketchpad = document.querySelector('#sketchpad');
 
-	$("#coloursbtn").click(function() { // Start the random colours grid
-		var res = parseInt(prompt("How many boxes high / wide do you want this thing?", "e.g. 16"));
-		if (res > 0) { // Ensures input is a valid integer before clearing and resetting
-			$("#sketchpad").empty();
-			gridinit(res);
-			startcolour();
-		}
-	});
-	$("#basicbtn").click(function() { // Start basic white-black grid
-		var res = parseInt(prompt("How many boxes high / wide do you want this thing?", "e.g. 16"));
-		if (res > 0) {
-			$("#sketchpad").empty();
-			gridinit(res);
-			startbasic();
-		}
-	});
-	$("#fadebtn").click(function() { // Start fading white-black grid
-		var res = parseInt(prompt("How many boxes high / wide do you want this thing?", "e.g. 16"));
-		if (res > 0) {
-			$("#sketchpad").empty();
-			gridinit(res);
-			startfade();
-		}
-	});
+let res = 16;
+gridinit(res);
+startbasic(); // On page load start the basic grid with size 16
+
+coloursbtn.addEventListener("click", function() { // Start the random colours grid
+	res = parseInt(prompt("How many boxes high / wide do you want the grid?", "e.g. 16"));
+	if (res > 0) { // Ensures input is a valid integer before clearing and resetting
+		sketchpad.replaceChildren();
+		gridinit(res);
+		startcolour();
+	}
+});
+basicbtn.addEventListener("click", function() { // Start basic white-black grid
+	res = parseInt(prompt("How many boxes high / wide do you want the grid?", "e.g. 16"));
+	if (res > 0) {
+		sketchpad.replaceChildren();
+		gridinit(res);
+		startbasic();
+	}
+});
+fadebtn.addEventListener("click", function() { // Start fading white-black grid
+	res = parseInt(prompt("How many boxes high / wide do you want the grid?", "e.g. 16"));
+	if (res > 0) {
+		sketchpad.replaceChildren();
+		gridinit(res);
+		startfade();
+	}
 });
 
-function gridinit(dim){
-	var boxsize = 650/dim; // Gets required box size by dividing grid size by user input value
+function gridinit(dim) {
+	let boxsize = 650 / dim; // Gets required box size by dividing grid size by user input value
 	console.log(boxsize);
-	for (var i = 0; i < dim*dim; i++) { // Makes all the squares you square
-		$("#sketchpad").append("<div class='box'></div>");
-	}
-	$(".box").css("height", boxsize); // Sets box size once divs are created (I'm sure there's a neater way to do this)
-	$(".box").css("width", boxsize);
+	for (let i = 0; i < dim * dim; i++) { // Makes all the squares you square
+		let box = document.createElement('div');
+		box.style.height = boxsize + "px";
+		box.style.width = boxsize  + "px";
+		box.style.opacity = 0;
+		box.classList.add("box");
+		sketchpad.appendChild(box);
+	};
 }
 
-function startcolour(){
-	$(".box").hover(function(){
-		var newcol = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
-		/* Above generates a random hex value - full disclosure, I have very little idea how it works but promise I will someday learn */
-		$(this).css("background-color",newcol);
-		$(this).css("opacity","1.2"); // Because default opacity is 0
-	})
+function startcolour() {
+	let boxes = document.querySelectorAll('.box');
+	boxes.forEach(box => box.addEventListener("mouseover", function () {
+		let newcol = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
+		// Above generates a random hex value
+		this.style.backgroundColor = newcol;
+		this.style.opacity = "1.2"; // Because default opacity is 0
+	}));
 }
 
-function startbasic(){
-	$(".box").hover(function(){
-		$(this).css("opacity","1.2"); // Changes div from white to black
-	})
+function startbasic() {
+	let boxes = document.querySelectorAll('.box');
+	boxes.forEach(box => box.addEventListener("mouseover", function () {
+		this.style.opacity = "1.2"; // Changes div from white to black
+	}));
 }
 
-function startfade(){
-	$(".box").hover(function(){
-		$(this).css("opacity", "+=0.1"); // Adds 10% of opacity, fading white to black
-	});
+function startfade() {
+	let boxes = document.querySelectorAll('.box');
+	boxes.forEach(box => box.addEventListener("mouseover", function () {
+		newOpacity = parseFloat(this.style.opacity) + 0.1;
+		this.style.opacity = newOpacity; // Adds 10% of opacity, fading white to black
+	}));
 }
